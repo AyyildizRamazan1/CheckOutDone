888548badeebe1175575e460fe3a8bf1df70f6dd
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-07-24 15:28:25 +0300

    User CRUD operations added to service and controller layers

--- src/main/java/com/ramazanayyildiz/CheckOutDone/controller/UsersController.java
+++ src/main/java/com/ramazanayyildiz/CheckOutDone/controller/UsersController.java
@@ -1,4 +1,44 @@
 package com.ramazanayyildiz.CheckOutDone.controller;
 
+import com.ramazanayyildiz.CheckOutDone.dto.UsersDto;
+import com.ramazanayyildiz.CheckOutDone.entity.Users;
+import com.ramazanayyildiz.CheckOutDone.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("check/users")// http://localhost:8080/check/users
 public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @GetMapping()
+    public ResponseEntity<List<Users>> getAllUsers() {
+        List<Users> usersList = usersService.getAllUsers();
+        return ResponseEntity.ok(usersList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(usersService.findUser(id));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<UsersDto> createUser(@RequestBody Users users) {//RequestBody gelen json verisini Java nesnesine çevirir
+        UsersDto createdUserDto = usersService.createUser(users);
+        return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);//Role kontrol yapılacak
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsersDto> updateUser(@PathVariable("id") Integer userId, @RequestBody UsersDto usersDto) {
+        UsersDto updatedUser = usersService.updateUser(userId, usersDto);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/ramazanayyildiz/CheckOutDone/service/UsersService.java
+++ src/main/java/com/ramazanayyildiz/CheckOutDone/service/UsersService.java
@@ -1,4 +1,46 @@
 package com.ramazanayyildiz.CheckOutDone.service;
 
+import com.ramazanayyildiz.CheckOutDone.dto.UsersDto;
+import com.ramazanayyildiz.CheckOutDone.entity.Users;
+import com.ramazanayyildiz.CheckOutDone.exception.GeneralException;
+import com.ramazanayyildiz.CheckOutDone.repository.IUsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsersService {
+
+    @Autowired
+    private IUsersRepository iUsersRepository;
+
+    public List<Users> getAllUsers() {
+        return iUsersRepository.findAll();
+    }
+
+
+    public UsersDto createUser(Users users) {
+        Users savedUser = iUsersRepository.save(users);
+        return new UsersDto(savedUser);
+    }
+
+    public Users findUser(Integer id) {
+        return iUsersRepository.findById(id)
+                .orElseThrow(() -> new GeneralException(id + " Id numaralı kullanıcı bulunamadı!"));
+    }
+
+
+    public UsersDto updateUser(Integer userId, UsersDto usersDto) {
+        Users existingUser = iUsersRepository.findById(userId)
+                .orElseThrow(() -> new GeneralException(userId + " Id numaralı kullanıcı bulunamadı!"));
+
+        existingUser.setUserName(usersDto.getUserName());
+        existingUser.setRole(usersDto.getRole());
+
+        Users updatedUser = iUsersRepository.save(existingUser);
+
+        return new UsersDto(updatedUser);
+    }
 }
